e818f5b59dd53b2384ead9f7cb49ee2d4d939c1a
Gentry Rolofson <xdarkicex@gmail.com>
2016-08-31 23:00:04 -0700

    tunning

--- Day3/Conditional/Conditional.java
+++ Day3/Conditional/Conditional.java
@@ -5,6 +5,7 @@ public class Conditional{
   Conditional(){ System.out.println("Main constructor"); }
   private Console console = System.console();
   String guess;
+  boolean debug;
   int answer;
   float number;
   int if_won;
@@ -12,10 +13,9 @@ public class Conditional{
   int difficulty = 1;
   int guess_num;
   public void set_difficulty(int difficulty){ this.difficulty = difficulty; }
-  public void set_guess_num(int guess_num){ this.guess_num = (difficulty*5); }
+  public void set_guess_num(int guess_num){ this.guess_num = ((int)Math.pow(difficulty, 2)); }
 
   public void init(){
-
     Continue = true;
     Writer.say("Guess a number");
     while (Continue) {
@@ -23,9 +23,12 @@ public class Conditional{
       set_guess_num(guess_num);
       Writer.say("1 Through "+guess_num);
     guess = console.readLine();
+
+
     number=Float.valueOf(guess);
     int x=(int)number;
     answer = (int) Math.floor(Math.random() * guess_num);
+
     if (x == answer){
       Writer.say("**** You win  ****");
       if_won = 1;
@@ -37,6 +40,7 @@ public class Conditional{
       Writer.say("Your guess was too high! "+"Answer: "+answer);
       if_won = 3;
     }
+
     try { Thread.sleep(1000); }
     catch(InterruptedException e) { }
       if (if_won == 1){
